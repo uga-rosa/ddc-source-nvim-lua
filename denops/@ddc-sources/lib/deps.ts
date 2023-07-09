35b48facaa7fe24e59fe00dbf98b668e7bc2cbba
uga-rosa <uga6603@gmail.com>
2023-07-09 16:07:31 +0900

    Update dependencies

--- denops/@ddc-sources/lib/deps.ts
+++ denops/@ddc-sources/lib/deps.ts
@@ -2,20 +2,20 @@ export {
   BaseSource,
   type DdcGatherItems,
   type Item,
-} from "https://deno.land/x/ddc_vim@v3.8.1/types.ts";
+} from "https://deno.land/x/ddc_vim@v3.9.0/types.ts";
 export {
   type GatherArguments,
   type OnCompleteDoneArguments,
-} from "https://deno.land/x/ddc_vim@v3.8.1/base/source.ts";
-export { type Denops, fn } from "https://deno.land/x/ddc_vim@v3.8.1/deps.ts";
+} from "https://deno.land/x/ddc_vim@v3.9.0/base/source.ts";
+export { type Denops, fn } from "https://deno.land/x/ddc_vim@v3.9.0/deps.ts";
 
 export * as api from "https://deno.land/x/denops_std@v5.0.1/function/nvim/mod.ts";
 
 export {
   LineContext,
   linePatch,
-} from "https://deno.land/x/denops_lsputil@v0.4.6/mod.ts";
+} from "https://deno.land/x/denops_lsputil@v0.5.3/mod.ts";
 
 // For test
 export { test } from "https://deno.land/x/denops_test@v1.4.0/mod.ts";
-export { assertEquals } from "https://deno.land/std@0.192.0/testing/asserts.ts";
+export { assertEquals } from "https://deno.land/std@0.193.0/testing/asserts.ts";
